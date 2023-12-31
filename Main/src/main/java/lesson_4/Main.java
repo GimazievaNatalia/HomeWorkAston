fcc712d065cad6a885dbb8677912682c1cb73f9c
Natalia <imsi88@mail.ru>
2023-12-31 13:43:47 +0400

    Задание_4

---
+++ Main/src/main/java/lesson_4/Main.java
@@ -0,0 +1,20 @@
+package lesson_4;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создание массива сотрудников
+        Person[] personArray = new Person[5];
+        personArray[0] = new Person("Ivanov Ivan", "Engineer", "ivivan@mailbox.com", "892312312", 30000, 30);
+        personArray[1] = new Person("Loshkin Pavel", "Engineer", "pashalo@mail.ru", "89166154123", 30000, 32);
+        personArray[2] = new Person("Koshkin Kir", "Director", "kirdir@mail.ru", "894551552365", 150000, 35);
+        personArray[3] = new Person("Gorbunova Maria", "Booker", "prostomaria@mailbox.ru", "89157158169", 40000, 30);
+        personArray[4] = new Person("Prohorov Stepan", "Engineer", "stepankaftan@mail.ru", "87456122365", 30000, 40);
+
+        // Вывод информации о сотрудниках
+        for (Person person : personArray) {
+            Person.showPersoninformation();
+        }
+
+    }
+}
+
