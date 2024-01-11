cb703f30f1c9223732f949ed5c6c062ffe264286
Natalia <imsi88@mail.ru>
2024-01-11 17:28:29 +0400

    Задание_5 поправленное до конца

--- Main/src/main/java/lesson_5/Cat.java
+++ Main/src/main/java/lesson_5/Cat.java
@@ -3,22 +3,17 @@ package lesson_5;
 public class Cat extends Animal {
 
     private static int totalCats = 0;
-    //  int boulFood = 18; //миска с едой
     private String name;
     private boolean fullnes = false;//сытость кота, изначально кот голодный
     private int howMuchCatEat;//сколько ест кот
 
+    public String getName() {
+        return name;
+    }
+
     public int getHowMuchCatEat() {
         return howMuchCatEat;
     }
-
-
-    //создание массива котов
-    private static Cat[] catArray = {
-            new Cat("Рыжик", false, 10),
-            new Cat("Мурзик", false, 5),
-            new Cat("Зевс", false, 3)
-    };
 
     public Cat(String name, boolean fullnes, int howMuchCatEat) {
         this.name = name;
@@ -46,16 +41,5 @@ public class Cat extends Animal {
         System.out.println((fullnes = bowl.getBowlFood() - howMuchCatEat >= 0) ? fullnes : (name + " остался голодным"));
     }
 
-    //public void addFood() { //добавляем еду в миску
-    //int leftovers = boulFood - howMuchCatEat; //остаток после еды
-    // System.out.println((leftovers >= 0) ? ("Миска с едой = " + (boulFood + leftovers)) : "Миска с едой = " + (boulFood += 10));
-    // }
 
-    public void eatFromBowlarraycat(Bowl bowl) { //массив котов кушает
-        int bowl2 = bowl.getBowlFood();
-        for (Cat cat : catArray) {
-            bowl2 -= howMuchCatEat;
-            System.out.println(bowl.getBowlFood() >= 0 ? (cat.name + " поел из миски") : (cat.name + " остался голодный"));
-        }
-    }
 }
--- Main/src/main/java/lesson_5/Main.java
+++ Main/src/main/java/lesson_5/Main.java
@@ -1,18 +1,28 @@
 package lesson_5;
 
 public class Main {
+    static Bowl bowl = new Bowl();
+
+    public static void eatFromBowlarraycat() { //массив котов кушает
+        Cat[] catArray = new Cat[3];
+        catArray[0] = new Cat("Рыжик", false, 10);
+        catArray[1] = new Cat("Мурзик", false, 5);
+        catArray[2] = new Cat("Зевс", false, 3);
+        int bowl2 = bowl.getBowlFood();
+        for (Cat cat1 : catArray) {
+            bowl2 -= cat1.getHowMuchCatEat();
+            System.out.println(bowl.getBowlFood() >= 0 ? (cat1.getName() + " поел из миски") : (cat1.getName() + " остался голодный"));
+        }
+    }
 
     public static void main(String[] args) {
         Dog dog = new Dog();
-        Bowl bowl = new Bowl();
-
         Cat cat = new Cat("Барсик", false, 8);
-
-
 
         Circle circle = new Circle("желтый", "голубой", 5);
         Triangle triangle = new Triangle("белый", "розовый", 1, 2, 3, 4);
         Rectangle rectangle = new Rectangle("черный", "красный", 2, 3);
+
 
         dog.run(200);
         dog.swim(30);
@@ -20,11 +30,10 @@ public class Main {
         cat.swim(100);
         cat.eatBowl(bowl);
         bowl.addFood(cat);
-        cat.eatFromBowlarraycat(bowl);
         System.out.println("Общее количество животных: " + Animal.getTotalAnimals());
         System.out.println("Количество созданных котов: " + Cat.getTotalCats());
         System.out.println("Количество созданных собак: " + Dog.getTotalDogs());
-
+        eatFromBowlarraycat();
 
         circle.getPerimetr();
         circle.getArea();
@@ -35,6 +44,7 @@ public class Main {
         triangle.getArea();
         triangle.getPerimetr();
         triangle.outputColor();
+
 
     }
 }
