1684b5a920fcb9ede3bed9d18911bca8e3d9b94d
Natalia <imsi88@mail.ru>
2024-01-16 21:48:28 +0400

    Задание_6 исправленное

--- Main/src/main/java/lesson_6/AppData.java
+++ Main/src/main/java/lesson_6/AppData.java
@@ -3,55 +3,66 @@ package lesson_6;
 import java.io.*;
 import java.util.Scanner;
 
-    public class AppData {
-        private String[] header;
-        private int[][] data;
+public class AppData {
+    private String[] header;
+    private int[][] data;
+    private File file;
 
-        public AppData(String[] header, int[][] data) {
-            this.header = header;
-            this.data = data;
+    public AppData(String[] header, int[][] data) {
+        this.header = header;
+        this.data = data;
+        this.file = null;
+    }
+
+
+       public void save() throws IOException { //записываем в файл
+        PrintWriter writer = null;
+
+           String relativePath = "resources/testtest1.csv";
+           this.file = new File(relativePath);
+
+           File parentDir = file.getParentFile();
+           if (!parentDir.exists() && !parentDir.mkdirs()) {
+               throw new IOException("Failed to create directory: " + parentDir);
+           }
+
+           // Создаем файл по относительному пути, если он не существует
+           if (file.createNewFile()) {
+               System.out.println("File created successfully at path: " + file.getAbsolutePath());
+           } else {
+               System.out.println("File already exists at path: " + file.getAbsolutePath());
+           }
+
+        try {
+            writer = new PrintWriter(file);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
-
-        public String[] getHeader() {
-            return header;
+        for (String line : this.header) { //записываем массив строк
+            writer.print(line);
+            writer.print(";");
         }
-
-        public int[][] getData() {
-            return data;
-        }
-
-        File file = new File("resources/test.csv");
-
-        public void save(String[] header, int[][] data) { //записываем в файл
-
-            PrintWriter writer = null;
-            try {
-                writer = new PrintWriter(file);
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
-            }
-            for (String line : header) { //записываем массив строк
-                writer.print(line);
+        writer.println();
+        for (int[] range : this.data) {  // записываем многомерный массив чисел
+            for (int number : range) {
+                writer.print(number);
                 writer.print(";");
             }
             writer.println();
-            for (int[] range : data) {  // записываем многомерный массив чисел
-                for (int number : range) {
-                    writer.print(number);
-                    writer.print(";");
-                }
-                writer.println();
-            }
-            writer.close();
         }
+        writer.close();
+    }
 
-        public void reading() { // читаем из файла
-            try (Scanner scanner = new Scanner(file)) {
-                while (scanner.hasNextLine()) {
-                    System.out.println(scanner.nextLine());
-                }
-            } catch (FileNotFoundException e) {
-                throw new RuntimeException(e);
+    public void reading() { // читаем из файла
+        if (file == null) {
+            throw new RuntimeException("File is not yet created or saved.");
+        }
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                System.out.println(scanner.nextLine());
             }
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
         }
     }
+}
--- Main/src/main/java/lesson_6/Main.java
+++ Main/src/main/java/lesson_6/Main.java
@@ -1,11 +1,13 @@
 package lesson_6;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         String[] header = {"value1", "value2", "value3"};
-        int[][] data = {{100, 200, 123}, {300, 400, 500}};
+        int[][] data = {{100, 200, 123}, {300, 400, 600}};
         AppData appData = new AppData(header, data);
-        appData.save(header, data);
+        appData.save();
         appData.reading();
     }
 }
