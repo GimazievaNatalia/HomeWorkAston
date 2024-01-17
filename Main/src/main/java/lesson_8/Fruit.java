cf785c5008e13f2eba01309f1a353c65b5cf5287
Natalia <imsi88@mail.ru>
2024-01-17 18:14:06 +0400

    Задание_8 1 часть

---
+++ Main/src/main/java/lesson_8/Fruit.java
@@ -0,0 +1,17 @@
+package lesson_8;
+
+
+public class Fruit  {
+    private String fruit;
+
+    public Fruit(String fruit) {
+        this.fruit = fruit;
+    }
+
+    public String getFruit() {
+        return fruit;
+    }
+}
+
+
+
