5907ecf92768e7d6f0f814d018d5f23f31f01848
Natalia <imsi88@mail.ru>
2024-01-21 17:19:24 +0400

    Задание_9 1 часть

--- Main/src/main/java/lesson_9/Main.java
+++ Main/src/main/java/lesson_9/Main.java
@@ -1,23 +1,32 @@
 package lesson_9;
 
-import java.util.stream.Stream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Main {
     public static void main(String[] args) {
-        Stream<Integer> randomNumbers = Stream.generate(() -> new Random().nextInt()).limit(10);
-
-                .filter(number -> number % 2 == 0)
-                // Печатаем четные числа (для примера)
-                .forEach(System.out::println);
-
-        // Определяем количество четных чисел
-        int count = Math.toIntExact(randomNumbers.limit(10)
-                .filter(number -> number % 2 == 0)
-                .count());
-
-        // Выводим результат
-        System.out.println("Количество четных чисел: " + count);
-
+        Random random = new Random();
+        IntStream randomNumbers = random.ints().limit(10)
+        .filter(number -> number % 2 == 0);
+        randomNumbers.forEach(System.out::println);
     }
 }
+
+
+
+
+               //
+                // Печатаем четные числа (для примера)
+               // .forEach(System.out::);
+
+        // Определяем количество четных чисел
+       // int count = Math.toIntExact(randomNumbers.limit(10)
+               // .filter(number -> number % 2 == 0)
+               // .count());
+
+        // Выводим результат
+      //  System.out.println("Количество четных чисел: " + count);
+
