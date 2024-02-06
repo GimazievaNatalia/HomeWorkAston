521bdfbe660643a39215a1fd5dd436e7527411aa
Natalia <imsi88@mail.ru>
2024-02-06 21:55:48 +0400

    Lesson_14

--- Main/src/main/java/org/example/gimazieva/Constan.java
+++ Main/src/main/java/org/example/gimazieva/Constan.java
@@ -1,7 +1,10 @@
 package org.example.gimazieva;
 
+;
+
 public class Constan {
     public static class TimeoutVariable {
         public static final int IMPLISIT_WAIT = 4;
+        public static final int EXPLISIT_WAIT = 5;
     }
 }
---
+++ Main/src/main/java/org/example/gimazieva/MtsBlockOnlinePayment.java
@@ -0,0 +1,9 @@
+package org.example.gimazieva;
+
+import org.openqa.selenium.WebDriver;
+
+public class MtsBlockOnlinePayment extends BasePage{
+    public MtsBlockOnlinePayment(WebDriver driver){
+        super(driver);
+    }
+}
